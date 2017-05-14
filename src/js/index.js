1e7e22fd63638f9a4a084c1ca443f3674f728aa2
Leszek Pawlak <leszek@bitnoi.se>
2017-05-14 19:11:05 +0200

    Add comments to index.js file

--- src/js/index.js
+++ src/js/index.js
@@ -10,9 +10,9 @@ import { createLogger } from 'redux-logger'
 import reducer from './reducers/index'
 // containers
 import Home from './containers/Home'
-// pages
+// support for old browsers
 import 'babel-polyfill'
-
+// logger for developers
 const logger = createLogger({ collapsed: true })
 
 const enhancer = compose(
@@ -23,9 +23,9 @@ const enhancer = compose(
     window.devToolsExtension ? window.devToolsExtension() : f => f
 )
 
-// Add the reducer to your store on the `routing` key
+// Add the reducer to your store
 const store = createStore(reducer, {}, process.env.NODE_ENV !== 'production' ? enhancer : applyMiddleware(thunk))
-
+// put react to #root div in the index.html file
 ReactDOM.render((
     <Provider store={store}>
         <Home />
