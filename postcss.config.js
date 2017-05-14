14f356ba427c448fdb2febff8cbec524d04951d0
Leszek Pawlak <leszek@bitnoi.se>
2017-05-14 12:28:42 +0200

    Add stylelint and custom media

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,21 @@
 module.exports = {
     plugins: [
         require('autoprefixer')({
-            browsers: [
-                '> 5%',
-                'last 2 versions',
-            ],
+            browsers: ['> 5%', 'last 2 versions'],
         }),
-        require('postcss-import')({
-            async: true,
-        }),
+        require('postcss-import')({async: true}),
         require('postcss-nested')(),
         require('postcss-extend')(),
         require('postcss-simple-vars')(),
         require('postcss-quantity-queries')(),
+        require('postcss-custom-media')({
+            extensions: {
+                '--phone': '(min-width: 544px)',
+                '--tablet': '(min-width: 768px)',
+                '--desktop': '(min-width: 992px)',
+                '--large-desktop': '(min-width: 1200px)',
+            },
+        }),
         require('postcss-font-magician')(),
     ],
 }
